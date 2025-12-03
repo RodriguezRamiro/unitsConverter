d3d1a43fe845bb19642dd1fa0c9557117b0aa666
Ramiro Rodriguez <rodriguezcodesolutions@gmail.com>
2025-12-02 22:24:50 -0500

    refactored ui layout, proper ids in document. adjusted styles to match and added dynamic button layout. completed variables and functions in script to perform intended functionalities and operations.

--- script.js
+++ script.js
@@ -1,7 +1,36 @@
+const userInput = document.getElementById('user-input')
 const convertBtn = document.getElementById('convertBtn')
-const unitInput = document.getElementById('units-input')
+const lengthOutput = document.getElementById('length-output')
+const volumeOutput = document.getElementById('volume-output')
+const massOutput = document.getElementById('mass-output')
+
+const measurements = {
+  length: 3.281,
+  volume: 3.785,
+  mass: 2.205,
+}
 
 convertBtn.addEventListener('click', function(){
-  let unit = Number(unitInput.value)
-  console.log(unit)
+  const unit = Number(userInput.value)
+  const metersToFt = unit * measurements.length
+  const feetToMt = unit / measurements.length
+  lengthOutput.textContent = `${unit} Meters = ${metersToFt.toFixed(3)} Feet | ${unit} Feet = ${feetToMt.toFixed(3)} Meters`
+
 })
+
+convertBtn.addEventListener('click', function(){
+  const unit = Number(userInput.value)
+  const literstoGal = unit / measurements.volume
+  const galToliters = unit * measurements.volume
+  volumeOutput.textContent = `${unit} Liters = ${literstoGal.toFixed(3)} Gallons | ${unit} Gallons = ${galToliters.toFixed(3)} Liters`
+
+})
+
+convertBtn.addEventListener('click', function(){
+ const unit = Number(userInput.value)
+ const kiloToPounds = unit * measurements.mass
+ const poundsToKilo = unit / measurements.mass
+ massOutput.textContent = `${unit} Kilograms = ${kiloToPounds.toFixed(3)}
+ Pounds | ${unit} Pounds = ${poundsToKilo.toFixed(3)} Kilograms`
+})
+ 
